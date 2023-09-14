27c5a1412eb490aecf2efc157b8fbb2c25faa588
jlorenzo-codeko <jlorenzo@codeko.com>
2023-09-14 09:55:03 +0200

    refactor: add info and check if JSON is valid
    refs: #WKOS-359

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,13 @@ function App() {
 }
 
   return (
-    <div className="App">
-
-<Controls text={JSON.stringify(inputJson)}/>
-
-
+    <div className='App' style={{ height: '100vh' }}>
+      <Controls text={JSON.stringify(inputJson)} />
+      <div>
+        Sistema de plantillas: <a href="https://github.com/guigrpa/docx-templates#writing-templates">https://github.com/guigrpa/docx-templates#writing-templates</a>
+      </div>
+      <div>El separador es <span style={{fontWeight: 'bold'}}>{'{{'}</span> y <span style={{fontWeight: 'bold'}}>{'}}'}</span>. 
+      Por ejemplo: {'{{ project.summary.title }}'}</div>
     </div>
   );
 }
--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -12,46 +12,57 @@ type TemplateState = {
     json: string,
 }
 
-const actionButton = (state: TemplateState) => {
-    if(state.file && state.file.length > 0) {
-        //window.alert(state.file[0] || "No hay archivo")
-    } else {
-        window.alert("No hay FileList")
-    }
-
-    if(state.file && state.file.length > 0 && state.file[0]) {
-        pruebaTemplate(state.file[0], state.json)
-    }
-}
-
 const pruebaTemplate = async (file: File, data: string) => {
     await processTemplate(file, data)
 }
 
 const Controls = ({ text }: Props) => {
     const templateState: TemplateState = {json: text}
-    const [state, setState] = useState(templateState);
+    const [state, setState] = useState(templateState)
+    const [error, setError] = useState(false)
+
+    const actionButton = (state: TemplateState) => {
+        try {
+            JSON.parse(state.json)
+            setError(false)
+
+            if(state.file && state.file.length > 0) {
+                //window.alert(state.file[0] || "No hay archivo")
+            } else {
+                window.alert("No se ha seleccionado un archivo")
+            }
+        
+            if(state.file && state.file.length > 0 && state.file[0]) {
+                pruebaTemplate(state.file[0], state.json)
+            }
+        } catch {
+            setError(true)
+        }    
+    }
 
     return (
         <>
-            <input
-                type='file'
-                id='template'
-                name='template'
-                accept='*'
-                onChange={e => setState({...state, file: e.target.files})}
-            />
-            <textarea
-                name="textValue"
-                value={state["json"]}
-                onChange={e => setState({...state, json: e.target.value})}
-            />
-            <button onClick={() => actionButton(state)}>
-                Aplicar
-            </button>
-        </>)
-
-
+            <div style={{ height: '50%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <input
+                    type='file'
+                    id='template'
+                    name='template'
+                    accept='*'
+                    onChange={e => setState({...state, file: e.target.files})}
+                />
+                <textarea
+                    name="textValue"
+                    value={state["json"]}
+                    onChange={e => setState({...state, json: e.target.value})}
+                    style={{ height: '60%', width: '50%', marginTop: 20, marginLeft: 50, marginRight: 50}}
+                />
+                <button onClick={() => actionButton(state)}>
+                    Aplicar
+                </button>
+            </div>
+            {error && <div style={{ color: 'red' }}>JSON no válido. <a href="https://jsonformatter.curiousconcept.com/">https://jsonformatter.curiousconcept.com/</a></div>}
+        </>
+    )
 }
 
 export default Controls
